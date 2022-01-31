849f59f20bab869e241baf3b3218d00efcb90a67
DomenicaC <dvintimillac1@est.ups.edu.ec>
2022-01-31 15:48:29 -0500

    Cargar Proyecto final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'registrarse',
+    loadChildren: () => import('./registrarse/registrarse.module').then( m => m.RegistrarsePageModule)
+  },
+  {
+    path: 'registrarse',
+    loadChildren: () => import('./pages/registrarse/registrarse.module').then( m => m.RegistrarsePageModule)
   }
 ];
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC8fqbmuSAf2yoX30tcJgmR5FSZeEVlQeE',
+    authDomain: 'proyectofinalpm-7815e.firebaseapp.com',
+    projectId: 'proyectofinalpm-7815e',
+    storageBucket: 'proyectofinalpm-7815e.appspot.com',
+    messagingSenderId: '657086690502',
+    appId: '1:657086690502:web:715c6518a9053d1f8713c7',
+  },
 };
